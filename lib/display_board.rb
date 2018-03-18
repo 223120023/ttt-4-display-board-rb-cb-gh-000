4697fe6dd2b80627294ec78c8e51584143b8f3fa
jane martin <223120023@student.unaki.ac.id>
2018-03-18 10:30:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  def spaceKosong(board[]="   ")
+  pembatasVertikal="|"
+  pembatasHorizontal="-----------"
+
+  puts spaceKosong(board[0])+pembatasVertikal+spaceKosong(board[1])+pembatasVertikal+spaceKosong(board[2])
+  puts pembatasHorizontal
+  puts spaceKosong(board[3])+pembatasVertikal+spaceKosong(board[4])+pembatasVertikal+spaceKosong(board[5])
+  puts pembatasHorizontal
+  puts spaceKosong(board[6])+pembatasVertikal+spaceKosong(board[7])+pembatasVertikal+spaceKosong(board[8])
+end 
+end 
+  
