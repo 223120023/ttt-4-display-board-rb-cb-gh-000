684a65c5abd1f082bbc7dc8540dce1127d3fcdb4
jane martin <223120023@student.unaki.ac.id>
2018-03-18 10:46:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board="   ")
+  pembatasVertikal="|"
+  pembatasHorizontal="-----------"
+
+  puts " #{board[0]} "+pembatasVertikal+" #{board[1]} "+pembatasVertikal+" #{board[2]} "
+  puts pembatasHorizontal
+  puts " #{board[3]} "+pembatasVertikal+" #{board[4]} "+pembatasVertikal+" #{board[5]} "
+  puts pembatasHorizontal
+  puts " #{board[6]} "+pembatasVertikal+" #{board[7]} "+pembatasVertikal+" #{board[8]} "
+end
